cfccb839f9529aa1721e721630d7828456ceb5c6
ggpradeep <80970765+ggpradeep@users.noreply.github.com>
2026-06-22 18:18:44 -0700

    Add files via upload

---
+++ break.py
@@ -0,0 +1,7 @@
+a = input("Enter a word")
+for i in a:
+    if (i == "A") or (i == "a"):
+        print("The letter A is in the word")
+        break
+    else:
+        print("A is not in the word")
