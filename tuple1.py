da1d3ce5e48ef370f1795957b393b9a95bab1767
ggpradeep <80970765+ggpradeep@users.noreply.github.com>
2026-07-07 18:38:04 -0700

    Add files via upload

---
+++ tuple1.py
@@ -0,0 +1,13 @@
+tuplex = ("Hi", 5.85, False, 3000)
+print(tuplex)
+tuple = (1000, 200, 4568, 1010)
+print(tuple)
+tuplex = tuple + (5,)
+print(tuplex)
+tuple_1 = (10, 20, 30, 20, 40, 20, 10, 20, 20)
+print(tuple_1.count(20))
+tuplle = (1, 1.1, 1.25, 2, 3, 4, 5.5, 10)
+slice = tuplle[3:5]
+print(slice)
+slice = tuplle[:6]
+print(slice)
